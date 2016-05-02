3baab3ac238cb2f767877da000845415a3b5a6e5
Ashik Khatri <ashikrk@gmail.com>
2016-05-02 17:02:47 -0500

    updated equation builder ui

--- FindTheRoot/app/src/main/java/edu/uh/findtheroot/EquationBuilderUI.java
+++ FindTheRoot/app/src/main/java/edu/uh/findtheroot/EquationBuilderUI.java
@@ -4,6 +4,7 @@ package edu.uh.findtheroot;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ public class EquationBuilderUI extends Activity {
             button7 , button8 , button9 , buttonAdd , buttonSub , buttonX,
             buttonExp, buttonDot , buttonC , buttonEqual;
     TextView eqTextView;
+    String equationText = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +39,139 @@ public class EquationBuilderUI extends Activity {
         buttonEqual = (Button) findViewById(R.id.buttoneql);
         eqTextView = (TextView) findViewById(R.id.eqView);
 
-        eqTextView.setText(Html.fromHtml("X<sup>2</sup>"));
+        equationText = "";
+
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "1";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(buttonExp.isSelected() == true) {
+                    equationText += "<sup>2</sup>";
+                    buttonExp.setSelected(false);
+                    updateButtonStatusOnExponent(true);
+                }
+                else {
+                    equationText += "2";
+                }
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "3";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "4";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "5";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "6";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "7";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "8";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "9";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "0";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        buttonX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += "x";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        buttonExp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonExp.setSelected(true);
+                updateButtonStatusOnExponent(false);
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += " + ";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationText += " - ";
+                eqTextView.setText(Html.fromHtml(equationText));
+            }
+        });
+        
+    }
+
+    private void updateButtonStatusOnExponent(boolean status) {
+        buttonAdd.setEnabled(status);
+        buttonSub.setEnabled(status);
+        button0.setEnabled(status);
+        buttonDot.setEnabled(status);
+        buttonX.setEnabled(status);
+        buttonEqual.setEnabled(status);
+        buttonExp.setEnabled(status);
+        buttonC.setEnabled(status);
     }
 }
