b9e15b3b292f7c6d82868a97f7116f99cee5293c
Ashik Khatri <ashikrk@gmail.com>
2016-05-05 18:06:09 -0500

    Created Secant activity

---
+++ FindTheRoot/app/src/main/java/edu/uh/findtheroot/SecantActivity.java
@@ -0,0 +1,13 @@
+package edu.uh.findtheroot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecantActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secant);
+    }
+}
